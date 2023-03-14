f07a08b937651a375b1a7a46fa256103864163f2
yoItsLeo <126665791+yoItsLeo@users.noreply.github.com>
2023-03-14 10:15:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function ShowAll(){
+    document.getElementById("idk").style.display = "block";
+    
+}
+
+function ShowKeepUp(){
+    document.getElementById("img2").style.display = "none";
+    document.getElementById("text").style.display = "none";
+}
+
+function ShowVideos(){
+    document.getElementById("Tag_content").innerHTML = "Videos"
+}
+
+function ShowScreenshots(){
+    document.getElementById("Tag_content").innerHTML = "Screenshots";
+}
+
+function ShowReview(){
+    document.getElementById("Tag_content").innerHTML = "Review";
+}
+
+function ShowDiscussions(){
+    document.getElementById("Tag_content").innerHTML = "Discussions";
+}
+
+
